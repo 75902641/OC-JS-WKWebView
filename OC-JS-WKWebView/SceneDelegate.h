32f7d2f2d2416c9bf411c0f542a076f306fcb861
75902641 <75902641@qq.com>
2019-10-14 09:59:58 +0800

    Create SceneDelegate.h

---
+++ OC-JS-WKWebView/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  OC-JS-WKWebView
+//
+//  Created by KT-yzx on 2019/10/14.
+//  Copyright © 2019 OC-JS-WKWebView. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
