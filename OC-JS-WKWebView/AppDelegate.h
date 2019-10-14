7875b60b65928a2b708f4c067cfde5b33addd63b
75902641 <75902641@qq.com>
2019-10-14 09:58:51 +0800

    Create AppDelegate.h

---
+++ OC-JS-WKWebView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OC-JS-WKWebView
+//
+//  Created by KT-yzx on 2019/10/14.
+//  Copyright © 2019 OC-JS-WKWebView. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
